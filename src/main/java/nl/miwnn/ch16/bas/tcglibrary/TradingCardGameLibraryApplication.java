6a1f01201011615dcbaa28f2cd0f77e43d40f3d0
Bas Folkers <bfolkers07@hotmail.com>
2025-06-11 17:44:11 +0200

    Starter project from Spring initializing

---
+++ src/main/java/nl/miwnn/ch16/bas/tcglibrary/TradingCardGameLibraryApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.bas.tcglibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingCardGameLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingCardGameLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch16/bas/tcglibrary/TradingCardGameLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.bas.tcglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingCardGameLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

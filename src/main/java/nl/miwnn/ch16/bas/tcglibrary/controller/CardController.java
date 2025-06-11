35261cbfbd4672c513a09ffadbe65aceb2c12990
Bas Folkers <bfolkers07@hotmail.com>
2025-06-11 20:15:02 +0200

    Add the ability to add cards through a form

--- src/main/java/nl/miwnn/ch16/bas/tcglibrary/controller/CardController.java
+++ src/main/java/nl/miwnn/ch16/bas/tcglibrary/controller/CardController.java
@@ -1,9 +1,13 @@
 package nl.miwnn.ch16.bas.tcglibrary.controller;
 
+import nl.miwnn.ch16.bas.tcglibrary.model.Card;
 import nl.miwnn.ch16.bas.tcglibrary.repositories.CardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
 
@@ -20,9 +24,27 @@ public class CardController {
         this.cardRepository = cardRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping({"/", "/card/overview"})
     private String showCardOverview(Model datamodel) {
         datamodel.addAttribute("allCards", cardRepository.findAll());
         return "cardOverview";
+    }
+
+    @GetMapping("/card/new")
+    private String showNewCardForm(Model datamodel) {
+        datamodel.addAttribute("formCard", new Card());
+
+        return "cardForm";
+    }
+
+    @PostMapping("/card/save")
+    private String saveOrUpdateCard(@ModelAttribute("formCard") Card cardToBeSaved, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.err.println(bindingResult.getAllErrors());
+        } else {
+            cardRepository.save(cardToBeSaved);
+        }
+
+        return "redirect:/card/overview";
     }
 }

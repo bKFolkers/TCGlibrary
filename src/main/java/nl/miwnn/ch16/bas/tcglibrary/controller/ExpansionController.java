e7cfbff19f59119c83f1bc9d8d9ca60a28f1db60
Bas Folkers <bfolkers07@hotmail.com>
2025-06-15 15:27:07 +0200

    Added update button to expansion overview

--- src/main/java/nl/miwnn/ch16/bas/tcglibrary/controller/ExpansionController.java
+++ src/main/java/nl/miwnn/ch16/bas/tcglibrary/controller/ExpansionController.java
@@ -1,11 +1,15 @@
 package nl.miwnn.ch16.bas.tcglibrary.controller;
 
+import nl.miwnn.ch16.bas.tcglibrary.model.Card;
+import nl.miwnn.ch16.bas.tcglibrary.model.Deck;
 import nl.miwnn.ch16.bas.tcglibrary.model.Expansion;
 import nl.miwnn.ch16.bas.tcglibrary.repositories.ExpansionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * @author Bas Folkers
@@ -32,6 +36,12 @@ public class ExpansionController {
         return "expansionForm";
     }
 
+    @GetMapping("/expansion/delete/{expansionId}")
+    private String deleteExpansion(@PathVariable("expansionId") Long expansionId) {
+        expansionRepository.deleteById(expansionId);
+        return "redirect:/expansion/overview";
+    }
+
     @PostMapping("/expansion/save")
     private String saveOrUpdateExpansion(@ModelAttribute("formExpansion") Expansion expansionToBeSaved,
                                          BindingResult bindingResult) {
@@ -44,9 +54,41 @@ public class ExpansionController {
         return "redirect:/expansion/overview";
     }
 
-    @GetMapping("/expansion/delete/{expansionId}")
-    private String deleteExpansion(@PathVariable("expansionId") Long expansionId) {
-        expansionRepository.deleteById(expansionId);
-        return "redirect:/expansion/overview";
-    }
+//    @GetMapping("/expansion/update/{expansionId}")
+//    private String updateExpansion(@PathVariable("expansionId") Long expansionId, Model datamodel) {
+//        Optional<Expansion> optionalExpansion = expansionRepository.findById(expansionId);
+//    }
+
+//    @GetMapping("/new/{expansionId}")
+//    private String createNewCard(@PathVariable("expansionId") Long expansionId) {
+//        Optional<Expansion> optionalExpansion = expansionRepository.findById(expansionId);
+//
+//        if (optionalExpansion.isPresent()) {
+//            Card card = new Card(optionalExpansion.get());
+//            cardRepository.save(card);
+//        }
+//        return "redirect:/card/new";
+//    }
+
+//    @GetMapping("/update/{deckId}")
+//    private String updateDeck(@PathVariable Long deckId, Model datamodel) {
+//        Deck deck = deckRepository.findById(deckId).orElseThrow();
+//        datamodel.addAttribute("formDeck", deck);
+//        datamodel.addAttribute("allCards", cardRepository.findAll());
+//        return "updateDeckForm";
+//    }
+
+//    @PostMapping("/update/save")
+//    private String saveUpdatedDeck(@ModelAttribute("formDeck") Deck formDeck, BindingResult result) {
+//        if (result.hasErrors()) {
+//            return "updateDeckForm";
+//        }
+//
+//        Deck deck = deckRepository.findById(formDeck.getDeckId()).orElseThrow();
+//        deck.getCards().addAll(formDeck.getCards());
+//        deckRepository.save(deck);
+//
+//        return "redirect:/deck/overview";
+//    }
+
 }
